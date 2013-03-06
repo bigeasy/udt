09b3c9c3cea314f737362e96f533028976a655d2
Alan Gutierrez <alan@prettyrobots.com>
2013-03-06 01:33:43 -0500

    Got it, I think.

--- t/sdk/proxy.t.js
+++ t/sdk/proxy.t.js
@@ -18,7 +18,8 @@ require('./proof')(3, function (step, say, ok, equal, execute, proxy) {
     });
   });
   server.on('close', function (code, signal) {
-    equal(code, 0, 'server closed');
+    if (code) equal(code, 0, 'server closed');
+    else equal(signal, 'SIGTERM', 'server closed');
     callback(null);
   });
 });
